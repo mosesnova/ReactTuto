87baa51eed101ecaf4dbceba95aad7e9d6cda6e8
Moses Nova <mosesabhisheknova@yahoo.com>
2023-04-07 05:34:12 +1200

    useEffect Hook

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+
 import { Layout } from './components/Layout';
 import './custom.css';
 import { FetchEmployee } from './components/Employee/FetchEmployee';
+//eslint-disable-next-line
 import { FetchCounter } from './components/Employee/FetchCounter';
+
 
 export default class App extends Component {
   static displayName = App.name;
+    
+    render() {
+       
+        return (
 
-  render() {
-    return (
       <Layout>
         <Routes>
           {AppRoutes.map((route, index) => {
@@ -20,8 +25,10 @@ export default class App extends Component {
                 <Route path='/FetchEmployee' element={<FetchEmployee />} />
                 <Route path='/FetchCounter' element={<FetchCounter />} />
                
-        </Routes>
+                </Routes>
+                
       </Layout>
+
     );
   }
 }
--- ClientApp/src/components/Employee/FetchCounter.js
+++ ClientApp/src/components/Employee/FetchCounter.js
@@ -1,15 +1,60 @@
-﻿import { useState } from 'react';
-
+﻿// eslint-disable-next-line
+import { useState, useEffect } from 'react';
+import { createConnection } from './Chat.js';
 export  function FetchCounter() {
     const [count, setCount] = useState(0);
-
+    const [roomId, setRoomId] = useState('general');
+    const [show, setShow] = useState(false);
     function handleClick() {
         setCount(count + 1);
     }
+   
+    function ChatRoom({ roomId }) {
+        const [serverUrl, setServerUrl] = useState('https://localhost:1234');
 
+        useEffect(() => {
+            const connection = createConnection(serverUrl, roomId);
+            connection.connect();
+            return () => {
+                connection.disconnect();
+            };
+        }, [roomId, serverUrl]);
+
+        return (
+            <>
+                <label>
+                    Server URL:{' '}
+                    <input
+                        value={serverUrl}
+                        onChange={e => setServerUrl(e.target.value)}
+                    />
+                </label>
+                <h1>Welcome to the {roomId} room!</h1>
+            </>
+        );
+    }
     return (
         <button onClick={handleClick}>
             You pressed me {count} times
+            <>
+                <label>
+                    Choose the chat room:{' '}
+                    <select
+                        value={roomId}
+                        onChange={e => setRoomId(e.target.value)}
+                    >
+                        <option value="general">general</option>
+                        <option value="travel">travel</option>
+                        <option value="music">music</option>
+                    </select>
+                </label>
+                <button onClick={() => setShow(!show)}>
+                    {show ? 'Close chat' : 'Open chat'}
+                </button>
+                {show && <hr />}
+                {show && <ChatRoom roomId={roomId} />}
+            </>
         </button>
+
     );
 }
